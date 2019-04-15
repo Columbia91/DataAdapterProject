2dcce4a6a1d6f81875e8687b4b60cff0ea7748db
Columbia91 <alpha.z91@mail.ru>
2019-04-15 21:08:18 +0600

    Добавьте файлы проекта.

---
+++ DataAdapterProject.DataAccess/UsersTableDataService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAdapterProject.DataAccess
+{
+    public class UsersTableDataService
+    {
+        private readonly string _connectionString;
+        private readonly string _providerName;
+        private DbProviderFactory _providerFactory;
+
+        public UsersTableDataService()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            _providerName = ConfigurationManager.ConnectionStrings["DefaultConnection"].ProviderName;
+            _providerFactory = DbProviderFactories.GetFactory(_providerName);
+        }
+    }
+}
